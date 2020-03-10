5be60a767a09056202e001517b70fcf58da0ec15
Mark Nemec <mrknmc@me.com>
2020-03-10 15:34:06 +0100

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -4,19 +4,25 @@ from setuptools import setup, find_packages
 VERSION = '1.0.1'
 setup(
     name='tel_grammar',
-    url='https://github.com/unite-io/tel_grammar/python',
     description='TEL Grammar parser in Python',
+    url='https://github.com/unite-io/tel_grammar/python',
+    project_urls={'Source Code': 'https://github.com/unite-io/tel_grammar/python'},
+    author="Panoramic",
+    maintainer="Panoramic",
+    keywords=["grammar", "taxonomy"],
     version=VERSION,
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[
-        'antlr4-python3-runtime==4.8',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    extras_require={
-        'dev': [
-            'pytest',
-        ],
-    },
-    # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=['Programming Language :: Python :: 3.6']
+    install_requires=['antlr4-python3-runtime==4.8'],
+    extras_require={'tests': ['pytest>=5.3.5'], 'dev': ['pytest>=5.3.5', 'pre-commit>=2.1.1.']},
+    include_package_data=True,
 )
